6bd30fc18d4f5c6909725447d040a3d1fa0c849c
Damian <damianjnc@gmail.com>
2019-11-29 11:15:13 +0000

    updates

--- server/app.js
+++ server/app.js
@@ -2,15 +2,27 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const app = express();
 
+// allow cross-origin request
+app.use(cors())
+
 // connect to mlab database
 // make sure to replace my db string & creds with your own
-mongoose.connect('mongodb://ninja:test@ds161148.mlab.com:61148/graphql-ninja')
+mongoose.connect(
+  'mongodb+srv://kodyczasupl1:patrycja1!AM@cluster0-g7ntl.mongodb.net/test?retryWrites=true&w=majority',
+  {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+  }
+)
+
 mongoose.connection.once('open', () => {
-    console.log('conneted to database');
-});
+    console.log('connected to the database')
+})
+
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
